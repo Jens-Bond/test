ebbe62b2b66f4d2830310d725fc97537862d3c78
Jens-Bond <115501160+Jens-Bond@users.noreply.github.com>
2022-10-10 21:21:28 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function getNumber() {
+    var input = document.getElementById('inputNumber').value;
+    //console.log(input);
+    //alert(input);
+
+    displayDetails(input);
+}
+
+function displayDetails(userPhone) {
+    console.log(userPhone);
+    var fullDiv = document.createElement('div');
+    fullDiv.className = "flexbox-container-2"
+}
