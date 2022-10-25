49f08772fe53c37ca8641edf61c6f0b7cc7cd41a
Jens-Bond <115501160+Jens-Bond@users.noreply.github.com>
2022-10-25 14:57:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ dict.push({
 function getHTML(link) {
   let PROXY = "https://ghg7femhx6.execute-api.us-east-1.amazonaws.com/";
   //let finalLink = proxy + link;
-  let finalLink = link;
+  let finalLink = PROXY + link;
   response = fetch(finalLink).then(response => response.text()).then((response) => {
       var parser = new DOMParser();
       var doc = parser.parseFromString(response, 'text/html');
       console.log(doc);
-      re = doc.getElementsByTagName('a');
-      console.log(re.getElementsByClassName('link-primary'));
+      re = doc.getElementsByClassName('link-primary');
+      console.log(re);
       //let element = doc.getElementsByClassName("link-primary")[0];
       //let href1 = element.getAttribute("href");
   }).catch(err => console.log(err))
