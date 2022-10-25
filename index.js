a7f222ed5aefbd16c58c507514c7ffd261936be7
Jens-Bond <115501160+Jens-Bond@users.noreply.github.com>
2022-10-25 18:13:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,15 +21,27 @@ function getHTML(link) {
   response = fetch(finalLink).then(response => response.text()).then((html) => {
       var parser = new DOMParser();
       var doc = parser.parseFromString(html, 'text/html');
-      console.log(doc);
-      let t = doc.getElementsByClassName("link-primary")[0];
-      console.log(typeof(t));
-      console.log(t)
-      let g = doc.getElementsByClassName("link-primary")[0].href;
-      console.log(g)
-      //let element = doc.getElementsByClassName("link-primary")[0];
-      //let href1 = element.getAttribute("href");
+      //console.log(doc);
+      let href = doc.getElementsByClassName("link-primary")[0].href;
+      console.log("person href:", href);
+      info.push({
+          "searchedLink": href
+      });
+      console.log("stored:", info);
   }).catch(err => console.log(err))
 };
+/*
+function getCODE(link) {
+  let PROXY = "https://ghg7femhx6.execute-api.us-east-1.amazonaws.com/";
+  //let finalLink = proxy + link;
+  let finalLink = PROXY + link;
+  response = fetch(finalLink).then(response => response.text()).then((html) => {
+      d
+  }).catch(err => console.log(err))
+};
+
+*/
+
+
 getHTML("https://www.merinfo.se/search?who=0702990271");
 
